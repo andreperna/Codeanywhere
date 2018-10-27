963d6fc9305164216b460c2293fc36d3e1c6d012
andreperna <andreperna@gmail.com>
2018-10-27 11:13:51 -0400

    .

--- Mensageiro.java
+++ Mensageiro.java
@@ -1,5 +1,5 @@
 public class Mensageiro {
 	public static void main(String[] args) {
-	System.out.println(" Programar e como andar em uma bicicleta pegando fogo\n");
+	System.out.println("ggg Programar e como andar em uma bicicleta pegando fogo\n");
 	}
 }
